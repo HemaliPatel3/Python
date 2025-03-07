1c36a9bc5c133e057f4e4c7c7f846c70e93a9f5b
Hemali Patel <hemalip3718@gmail.com>
2025-03-07 12:42:08 +0530

    Update Janmansh and Assignments.py

--- codechef_python/Janmansh and Assignments.py
+++ codechef_python/Janmansh and Assignments.py
@@ -4,3 +4,22 @@ for i in range(int(input())):
         print("YES")
     else:
         print("NO")
+
+
+
+# using OOPS method 
+class Ass:
+    def __init__(self,a):
+        self.a = a
+    def cal(self):
+        if 10 -self.a >=3:
+            return 'YES'
+        else:
+            return 'NO'
+def main():
+    for i in range(int(input())):
+        a = int(input())
+        ass = Ass(a)
+        print(ass.cal())
+if __name__ == "__main__":
+    main()
