c050f3da6ad487f8f28f69458317da6579016cc9
Hemali Patel <hemalip3718@gmail.com>
2025-03-09 10:58:28 +0530

    Update GreaterAverage.py

--- codechef_python/GreaterAverage.py
+++ codechef_python/GreaterAverage.py
@@ -4,3 +4,25 @@ for i in range(int(input())):
         print('yes')
     else :
         print('no')
+
+
+
+# using OOPs concepts
+
+class GRE:
+    def __init__(self,a,b,c):
+        self.a = a
+        self.b = b
+        self.c = c
+    def cal(self):
+        if (self.a+self.b)/2 >self.c:
+            return 'yes'
+        else :
+            return 'no'
+def main():
+    for i in range(int(input)):
+    a,b,c = map(int,input().self())
+    gre = GRE(a,b,c)
+    print(gre.cal())
+if __name__ == '__main__':
+    main()
