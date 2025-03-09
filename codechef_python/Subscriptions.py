22dae27bfa36fa1fcfed3eb4ccd94d9d03d0b3f0
Hemali Patel <hemalip3718@gmail.com>
2025-03-09 11:05:19 +0530

    Update Subscriptions.py

--- codechef_python/Subscriptions.py
+++ codechef_python/Subscriptions.py
@@ -5,3 +5,19 @@ for i in range(int(input())):
     a =math.ceil(a/6)
     print(a*b)
     
+# using OOPS concepts
+import math
+class Sub():
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    def cal(self):
+        a = math.ceil(a/6)
+        return a*b
+def main():
+    for i in range(int(input())):
+        a,b = map(int,input().split())
+        sub = Sub(a,b)
+        print(sub.cal())
+if __name__ = '__main__':
+    main()
